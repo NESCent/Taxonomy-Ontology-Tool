61aecfcf2afa390bd8463532256b066617d8b8be
Jim Balhoff <balhoff@nescent.org>
2013-02-26 13:28:43 -0600

    Updated run script.

--- buildfiles/TaxonomyTool.sh
+++ buildfiles/TaxonomyTool.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-TAXONOMY_SCRIPT="../scripts/PBDBbuild_Oct_2012.xml"
-java -Xmx2500M -cp .:../jars/bbop.jar:../jars/jena-2.6.3.jar:../jars/junit.jar:../jars/log4j-1.2.15.jar:../jars/obo.jar:../jars/owlapi-bin.jar:../jars/mysql-connector-java-5.1.21.bin.jar org/nescent/VTO/VTOTool $TAXONOMY_SCRIPT
+TAXONOMY_SCRIPT="../scripts/PBDBbuild_Jan_2013.xml"
+java -Xmx2500M -cp .:../jars/bbop.jar:../jars/jena-2.6.3.jar:../jars/junit.jar:../jars/log4j-1.2.15.jar:../jars/obo.jar:../jars/owlapi-bin.jar:../jars/mysql-connector-java-5.1.21.bin.jar:../jars/VTOTool.jar org.nescent.VTO.VTOTool $TAXONOMY_SCRIPT
